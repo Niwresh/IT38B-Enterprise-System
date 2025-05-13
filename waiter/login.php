0c2d8142d7f0b5cc1fdd99b5f71bcbfbfdbc7ebb
Niwresh <sherwinlumakang827@gmail.com>
2025-05-13 23:22:15 +0800

    modified the CSS style in my waiter side

--- waiter/login.php
+++ waiter/login.php
@@ -103,7 +103,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         /* Additional button style */
         .modal button {
-            background-color:rgb(255, 120, 41);
+            background-color: #00B1FD;
             color: white;
             padding: 10px 20px;
             border: none;
